6e0a77f9e426d008cb78c8f18612f9782e9fdc41
Furby <jessfairbairn91@gmail.com>
2016-09-25 21:39:08 +0100

    new check box for adding tag when you only show one

--- services/taskAdder.js
+++ services/taskAdder.js
@@ -12,6 +12,32 @@ angular.module("wowApp").service('taskAdder', function(){
 		}
 	};
 
+	this.addTagChange = function(event, newTask){
+		if(event.target.checked){
+			newTask.tags = [this.tagShowing];
+		}else{
+			newTask.tags = [];
+		}
+	};
+
+	this.showAddTag = function(parentScope){
+		var tagMap = parentScope.tagMap;
+
+		var tagNames = Object.getOwnPropertyNames(tagMap);
+		var selectedTags = tagNames.filter(function(name){
+          return tagMap[name];
+      	});
+    	if(selectedTags.length === 1){
+    		this.tagShowing = selectedTags[0];
+    		return true;
+    	}else{
+    		this.tagShowing = null;
+    		return false;
+    	}
+	};
+
+	this.tagShowing = null;
+
 }).directive('addTask',function(){
   return{
     templateUrl : "directives/addTask.html",
